b7cbf27efc693cc2e9cc161dd547ed5025b0ac92
Jake Goulding <jake.goulding@gmail.com>
2012-10-06 10:40:16 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapybaraRace::Application.config.secret_token = '4f62e2d98512e11372039108aae99a80eeaad4269f1f4a0ca12573922e3660c869d3d31d16f74e08b9b9d7c009fd51b3b68e16cb7104a5a66accd18ab7e01478'
